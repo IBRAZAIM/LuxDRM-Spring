7f60b8f60252286dd034a4b2eef7e34ed52ef5d8
IBRAZAIM <anicrager9@gmail.com>
2024-06-30 16:23:01 +0600

    ""Refined styling for main page layout""

--- src/main/java/kz/ibrazaim/catalog/controller/Controller.java
+++ src/main/java/kz/ibrazaim/catalog/controller/Controller.java
@@ -1,13 +1,23 @@
 package kz.ibrazaim.catalog.controller;
 
+import kz.ibrazaim.catalog.model.Product;
+import kz.ibrazaim.catalog.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @org.springframework.stereotype.Controller
+@RequiredArgsConstructor
 @RequestMapping("/main")
 public class Controller {
+    private final ProductService productService;
     @GetMapping
-    public String getPage(){
+    public String getProducts(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
         return "LUXDRM";
     }
 }
